a0123df32c02c9b78574453217a67782cd2b62f3
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2018-03-15 20:12:29 +0100

    calc change

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -8,9 +8,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        Calculator calculator = new Calculator();
+        Calculator calc = new Calculator();
         RandomGenerator randomGenerator = new RandomGenerator();
-        System.out.println(calculator.sum(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
-        System.out.println(calculator.sub(15, 10));
+        System.out.println(calc.sum(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
+        System.out.println(calc.sub(15, 10));
     }
 }

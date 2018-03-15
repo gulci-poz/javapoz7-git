d8c13cf7ba55bdd3294f1feaaa0c51628fe53c00
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2018-03-15 20:34:43 +0100

    factorial added to Calculator class (#2)

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -8,9 +8,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        Calculator calc = new Calculator();
+        Calculator calculator = new Calculator();
         RandomGenerator randomGenerator = new RandomGenerator();
-        System.out.println(calc.sum(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
-        System.out.println(calc.sub(15, 10));
+        System.out.println(calculator.sum(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
+        System.out.println(calculator.sub(15, 10));
+        System.out.println(calculator.factorial(5));
     }
 }
--- src/sda/calculator/Calculator.java
+++ src/sda/calculator/Calculator.java
@@ -9,4 +9,15 @@ public class Calculator {
     public int sub(int a, int b) {
         return a - b;
     }
+
+    public int factorial(int n) {
+        int result = 1;
+
+        while (n > 1) {
+            result *= n;
+            n--;
+        }
+
+        return result;
+    }
 }
